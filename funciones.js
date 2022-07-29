f66e58e3e9befdd6c53124659b875fbd0361a1b7
Gaston Romero <romero_gaston_@hotmail.com>
2022-07-29 20:41:39 -0300

    segunda entrega del trabajo aplicando funciones

--- funciones.js
+++ funciones.js
@@ -1,5 +1,7 @@
+
 //TP ENTREGABLE
 
+/*
 alert("Bienvenidos a la simulación de cobro por tarjeta de crédito, rogamos por favor no ingresar datos personales reales.");
 alert("A continuación, vamos a solicitar algunos datos, por favor completar para continuar.");
 
@@ -16,28 +18,37 @@ while (nombre == ""|| apellido == "" || isNaN(nroTarj) || isNaN(vencimiento) || 
   nroTarj = parseInt(prompt("ingrese los 16 numeros de la tarjeta"));
   vencimiento = parseInt(prompt("ingrese fehca de vencimiento mm/yy"));
   ccv = parseInt(prompt("ingrese su codigo de seguridad"));
-}
+};
+
+
+
 
 alert("Vamos a realizar el cobro por ARS $10.000,00");
-alert("Ingrese la cantidad de cuotas que desea.\n CUOTAS 1= INTERES 0%.\n CUOTAS 3= INTERES 2%.\n CUOTAS 6= INTERES 5%.");
-let numero=parseInt(prompt("Ingrese cantidad de cuotas"));
-let precio = 10000;
 
-while(numero){
-   switch(numero) {
-     case "1":
-      alert("$" + precio + " Este es su monto a pagar");
-         break;
-     case "3":
-      alert("$" + precio * 1.02 + " Este es su monto a pagar");
-         break;
-     case "6":
-      alert("$" + precio * 1.05 + " Este es su monto a pagar");
-         break;
-     default:
-       alert("ERROR NUMERO DE CUOTA INVALIDA");
-       numero=parseInt(prompt("Ingrese cantidad de cuotas"));
-         break;
-   }
- numero = prompt("Ingrese un numero");
-}
+alert("Ingrese la cantidad de cuotas que desea.\n CUOTAS 1= INTERES 0%.\n CUOTAS 2= INTERES 20%.\n CUOTAS 3= INTERES 50%.");
+*/
+let numerox = parseInt(prompt(" Ingrese la cantidad de cuota en numero"));
+let precio = 10000;
+let cuota1 = 1;
+let cuota2 = 1.2;
+let cuota3 = 1.5;
+
+      function multiplicar(precio,numerox){
+  
+        var resultado= precio * numerox;
+        return(" Usted abonara $" + resultado );
+      }
+
+
+      if(numerox == "1"){
+      alert(multiplicar(precio,cuota1)+ "en 1 pago.");
+      }
+      if(numerox == "2"){
+      alert(multiplicar(precio,cuota2)+" en 2 cuotas.");
+      }
+      if( numerox == "3"){
+      alert(multiplicar(precio,cuota3)+" en 3 cuotas.");
+      }
+
+
+alert("MUCHAS GRACIAS POR SU COMPRA LO ESPERAMOS NUEVAMENTE!");
